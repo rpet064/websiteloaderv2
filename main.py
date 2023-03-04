6f72238de30d4efd8cf03a2dd2f7314eb5fccc9e
rpet064 <93119631+rpet064@users.noreply.github.com>
2023-03-04 22:48:08 +1300

    programme can now extract urls and open in driver

--- main.py
+++ main.py
@@ -1,9 +1,58 @@
 from selenium import webdriver
+from time import sleep
+from url import url_dict
+
+driver = webdriver.Chrome()
+
 
 def help():
+    # This function prints out the technical guide for different website options 
+
     help_text = open('./help.txt', 'r')
     print(help_text.read())
     help_text.close()
+
+
+def open_tab(url, tab_index):
+    driver.execute_script(f"window.open('{url[0]}')")
+    driver.switch_to.window(driver.window_handles[tab_index])
+    sleep(1000)
+    # //*[@id="identifierId"].sendkeys()
+    # //*[@id="identifierNext"]/div/button.click
+
+def extract_url(user_input):
+    # This function extracts the user inputs and sends the urls to the open website function
+
+    current_tab = 1
+    for input in user_input:
+        match input.lower():
+            case "y":
+                curr_urls = [value for key, value in url_dict.items() if 'y' in key][0]
+                open_tab(curr_urls, current_tab)
+                current_tab+=1
+            
+            case "p":
+                curr_urls = [value for key, value in url_dict.items() if 'p' in key][0]
+                open_tab(curr_urls, current_tab)
+                current_tab+=1
+
+            
+            case "c":
+                curr_urls = [value for key, value in url_dict.items() if 'c' in key][0]
+                open_tab(curr_urls, current_tab)
+                current_tab+=1
+
+
+            case "h":
+                curr_urls = [value for key, value in url_dict.items() if 'h' in key][0]
+                open_tab(curr_urls, current_tab)
+                current_tab+=1
+
+            case "g":
+                curr_urls = [value for key, value in url_dict.items() if 'g' in key][0]
+                open_tab(curr_urls, current_tab)
+                current_tab+=1
+
 
 def main():
     user_input = input("Insert what websites you would like to open, or type 'help'\n")
@@ -11,10 +60,7 @@ def main():
     if user_input == "help":
         help()
     else:
-        driver = webdriver.Chrome()
-        for i in range(len(user_input)):
-            driver.execute_script(f"window.open('{youtube_login}')")
-            driver.switch_to.window(driver.window_handles[i])
+        extract_url(user_input)
 
 if __name__ == "__main__":
     main()
---
+++ url.py
@@ -0,0 +1,22 @@
+url_dict = {
+    
+    'y':[ 
+        "https://accounts.google.com/ServiceLogin/signinchooser?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=en&ec=65620&ifkv=AWnogHf2WfiAzU8rBM58rXzB2XO8RtPCrmw8UP7XFSeLPsyTWSilLrRcHAXAqNrNrsUEe8MhE3xWIg&flowName=GlifWebSignIn&flowEntry=ServiceLogin",
+        "https://www.youtube.com/watch?v:Yxd-X8P-GtY&list:PLucn9CiuPwuaWKEeWSzfcq8Z4Q9ds7Q-G"
+        ],
+
+    'p':[
+        "https://portswigger.net/users",
+        "https://portswigger.net/web-security/all-materials"
+        ],
+
+    'c':[
+        "https://iam.auckland.ac.nz/profile/SAML2/Redirect/SSO?execution:e1s1",
+        "https://canvas.auckland.ac.nz/courses/86703"
+        ],
+    
+    'g':[
+        "https://github.com/login", 
+        "https://github.com/rpet064?tab:repositories"
+        ]
+}
